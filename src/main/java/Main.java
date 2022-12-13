9fa8131302b56b5d40a53738e7f296dc932a1b4d
Skyfall756 <110997330+Skyfall756@users.noreply.github.com>
2022-12-13 13:53:50 +0300

    add try

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -13,28 +14,28 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setConnectTimeout(5000)
                         .setSocketTimeout(30000)
                         .setRedirectsEnabled(false)
                         .build())
-                .build();
+                .build()
+        ) {
 
-        HttpGet request = new HttpGet(
-                "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+            HttpGet request = new HttpGet(
+                    "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
 
-        CloseableHttpResponse response = httpClient.execute(request);
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
 
-        List<Post> posts = mapper.readValue(response.getEntity().getContent(),
-                new TypeReference<>() {
-                });
-        posts.stream()
-                .filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
-                .forEach(System.out::println);;
+                List<Post> posts = mapper.readValue(response.getEntity().getContent(),
+                        new TypeReference<>() {
+                        });
+                posts.stream()
+                        .filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
+                        .forEach(System.out::println);
 
-
-        response.close();
-        httpClient.close();
+            }
+        }
     }
 }
